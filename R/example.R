96e6fc3c1896e9c3b2855fa1ccb1daf6a7d18e9b
Vincenzo Coia <vincenzo.coia@gmail.com>
2025-11-03 13:39:31 +0100

    Document example_function() to get check() to pass.

--- R/example.R
+++ R/example.R
@@ -1,6 +1,13 @@
-
-
-
+#' Example Function
+#'
+#' Example paragraph describing
+#' what the function does.
+#'
+#' @param x An input parameter.
+#' @return What gets returned by the function?
+#' @examples
+#' example_function(5)
+#' @export
 example_function <- function(x) {
   a <- 5
   x^2 * a
